ca1bcfdeeca53d4477035240f0f676f24018b861
iggy <spetsovich@gmail.com>
2016-08-28 20:13:31 -0400

    First commit

---
+++ includes/class-meetup-rsvp-publisher-activator.php
@@ -0,0 +1,36 @@
+<?php
+
+/**
+ * Fired during plugin activation
+ *
+ * @link       http://igorcodes.com
+ * @since      1.0.0
+ *
+ * @package    Meetup_Rsvp_Publisher
+ * @subpackage Meetup_Rsvp_Publisher/includes
+ */
+
+/**
+ * Fired during plugin activation.
+ *
+ * This class defines all code necessary to run during the plugin's activation.
+ *
+ * @since      1.0.0
+ * @package    Meetup_Rsvp_Publisher
+ * @subpackage Meetup_Rsvp_Publisher/includes
+ * @author     Igor Lubinets <igor@webilect.com>
+ */
+class Meetup_Rsvp_Publisher_Activator {
+
+	/**
+	 * Short Description. (use period)
+	 *
+	 * Long Description.
+	 *
+	 * @since    1.0.0
+	 */
+	public static function activate() {
+
+	}
+
+}
---
+++ includes/class-meetup-rsvp-publisher-deactivator.php
@@ -0,0 +1,36 @@
+<?php
+
+/**
+ * Fired during plugin deactivation
+ *
+ * @link       http://igorcodes.com
+ * @since      1.0.0
+ *
+ * @package    Meetup_Rsvp_Publisher
+ * @subpackage Meetup_Rsvp_Publisher/includes
+ */
+
+/**
+ * Fired during plugin deactivation.
+ *
+ * This class defines all code necessary to run during the plugin's deactivation.
+ *
+ * @since      1.0.0
+ * @package    Meetup_Rsvp_Publisher
+ * @subpackage Meetup_Rsvp_Publisher/includes
+ * @author     Igor Lubinets <igor@webilect.com>
+ */
+class Meetup_Rsvp_Publisher_Deactivator {
+
+	/**
+	 * Short Description. (use period)
+	 *
+	 * Long Description.
+	 *
+	 * @since    1.0.0
+	 */
+	public static function deactivate() {
+
+	}
+
+}
