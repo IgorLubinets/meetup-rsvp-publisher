e496c5b5cb84934bd19fd7e807d6c4a8baf9a270
iggy <spetsovich@gmail.com>
2016-09-29 02:09:18 -0400

    Cleaned up some css

--- admin/partials/meetup-rsvp-publisher-admin-settings.php
+++ admin/partials/meetup-rsvp-publisher-admin-settings.php
@@ -24,29 +24,6 @@
 
 	</h2>
 
-
-	<?php //NOTE: Need to move styles out into a separate stylesheet file ?>
-
- 	<style>
-		.admin-rsvp-card-container { border-radius: 20px; background-color: #f20017; 
-					padding: 0; padding-top: 35px; padding-bottom: 20px; margin-left: 10px;
-					width: 62%; max-width: 400px; min-width: 290px; text-align: center; }
-		#sortable { list-style-type: none; margin: 0; padding: 0; width: 100%; max-width: 400px; 
-				min-width: 290px; margin: 0 auto; }
-		#sortable li { margin: 0; padding: 0.4em; padding-left: 1.5em; padding-top: 20px; padding-bottom: 20px;
-				font-size: 1.4em; height: 35px; border: 1px solid rgba(0,0,0,.2); text-align: left; cursor: move; 
-				position: relative; }
-		#sortable li:hover { background: rgba(242,0,23, .78); color: white; }
-/*		#sortable li span { position: absolute; margin-left: -1.3em; } */
-		#sortable li input[type="checkbox"] { margin: 0; padding: 0; margin-right: 20px; float: right; }
-		#sortable li input[type="text"] { margin-left: 80px; position: absolute;  }
-		.admin-rsvp-card-title { color: gray; position: absolute; top: 50%; transform: translateY(-50%); }
-		.admin-rsvp-card-eyes { padding: 0px; font-size: 35px; text-align: center; margin: 0 auto; 
-				color: gray; float: right; margin-right: 30px; } 
-		.rsvp-fields-make-invisible { display: block }
-		.rsvp-fields-make-visible { display: none }
-	</style>
-
 	<h3>Drag to reorder fields</h3>
 
 	<div class="admin-rsvp-card-container">
@@ -55,7 +32,7 @@
 		<?php
 		$rsvpFieldOrder = get_option( 'meetup-rsvp-publisher_rsvp_field_order' );
 		$showHideRsvpFields = (array)get_option( 'webilect_meetup_rsvp_publisher_options_show_hide_rsvp_fields_list' );
-		var_dump($showHideRsvpFields);
+		//var_dump($showHideRsvpFields);
 
 		foreach( $rsvpFieldOrder as $currentField ) : ?>
 			<?php 
