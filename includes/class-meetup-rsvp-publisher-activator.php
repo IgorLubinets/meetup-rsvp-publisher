3d6095861ecc82651e16ee6fcb7d7f9269211081
iggy <spetsovich@gmail.com>
2016-10-05 05:54:40 -0400

    Still fixing the Rsvp field reorder bug

--- includes/class-meetup-rsvp-publisher-activator.php
+++ includes/class-meetup-rsvp-publisher-activator.php
@@ -31,6 +31,30 @@ class Meetup_Rsvp_Publisher_Activator {
 	 */
 	public static function activate() {
 
+		/*
+		$rsvpFieldOrder = get_option( 'meetup-rsvp-publisher_rsvp_field_order' );
+		if($rsvpFieldOrder === false) {
+			$rsvpFieldOrder = array( 
+				'rsvp-fields-date', 
+				'rsvp-fields-event-title', 
+				'rsvp-fields-address', 
+				'rsvp-fields-hostedby', 
+				'rsvp-fields-details' 
+				);
+		}
+		
+		$showHideRsvpFields = (array)get_option( 'webilect_meetup_rsvp_publisher_options_show_hide_rsvp_fields_list' );
+		if($showHideRsvpField === false) {
+			$rsvpFieldOrder = array( 
+				'rsvp-fields-date', 
+				'rsvp-fields-event-title', 
+				'rsvp-fields-address', 
+				'rsvp-fields-hostedby', 
+				'rsvp-fields-details' );
+		}
+		*/
+
+
 	}
 
 }
